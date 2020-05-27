9d8834ddc375d0ff79572506b350ede2d741fbdf
Ryan <thewebcoderz@gmail.com>
2020-05-26 23:38:40 -0400

    Testing congrats component. Also removing some repetative tests and putting them in a test folder

--- src/Jotto/Jotto.test.js
+++ src/Jotto/Jotto.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 
+import { findByTestAttr } from '../test/testUtils'
 import Jotto from './Jotto';
 
 Enzyme.configure({adapter: new EnzymeAdapter()});
@@ -10,10 +11,6 @@ const setup = (props={}, state=null) => {
     const wrapper = shallow(<Jotto {...props}/>)
     if (state) wrapper.setState(state);
     return wrapper
-}
-
-const findByTestAttr = (wrapper, val) => {
-    return wrapper.find(`[data-test='${val}']`)
 }
 
 test('renders wihtout error', () => {
--- src/Jotto/components/Congrats.test.js
+++ src/Jotto/components/Congrats.test.js
@@ -2,6 +2,27 @@ import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 
+import { findByTestAttr } from '../../test/testUtils'
 import Congrats from './Congrats'
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+const setup = (props={}, state=null) => {
+    const wrapper = shallow(<Congrats {...props}/>)
+    if (state) wrapper.setState(state);
+    return wrapper
+}
+
+test('renders wihtout error', () => {
+    const wrapper = setup()
+    const appComponent = findByTestAttr(wrapper, 'component-congrats');
+    expect(appComponent.length).toBe(1);
+});
+
+test('renders no text when `success` prop is false', () => {
+    const wrapper = setup({ success: false });
+});
+
+test('renders non-empty congrats message when `success` prop is true', () => {
+
+});
