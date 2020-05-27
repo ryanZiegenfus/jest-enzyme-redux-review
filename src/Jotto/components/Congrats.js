bc78d4febeb18ff48a89b007f67cef1ac00768b9
Ryan <thewebcoderz@gmail.com>
2020-05-26 23:58:25 -0400

    tested state and conditional rendering in Congrats component

--- src/Jotto/components/Congrats.js
+++ src/Jotto/components/Congrats.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
-export default () => {
-    return(
-        <div data-test="component-congrats"></div>
-    );
+export default (props) => {
+    if (props.success) {
+        return(
+            <div data-test="component-congrats">
+                <span data-test="congrats-message">
+                    Congratulations! You guessed the word!
+                </span>
+            </div>
+        )
+    } else {
+        return(
+            <div data-test="component-congrats" />
+        )
+    }
 }
