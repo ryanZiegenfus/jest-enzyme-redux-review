502295fe6145bd221c4c8299708ed1439c227d92
Ryan <thewebcoderz@gmail.com>
2020-05-27 01:26:06 -0400

    implemented PropType and checkPropType tests and did some dry refactoring of defaultProps

--- src/Jotto/components/Congrats.test.js
+++ src/Jotto/components/Congrats.test.js
@@ -2,19 +2,23 @@ import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 
-import { findByTestAttr } from '../../test/testUtils'
+
+import { findByTestAttr, checkProps } from '../../test/testUtils'
 import Congrats from './Congrats'
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+const defaultProps = { success: false }
+
 const setup = (props={}, state=null) => {
-    const wrapper = shallow(<Congrats {...props}/>)
+    const setupProps = {...defaultProps, ...props}
+    const wrapper = shallow(<Congrats {...setupProps}/>)
     if (state) wrapper.setState(state);
     return wrapper
 }
 
 test('renders wihtout error', () => {
-    const wrapper = setup()
+    const wrapper = setup({ success: false })
     const appComponent = findByTestAttr(wrapper, 'component-congrats');
     expect(appComponent.length).toBe(1);
 });
@@ -26,7 +30,12 @@ test('renders no text when `success` prop is false', () => {
 });
 
 test('renders non-empty congrats message when `success` prop is true', () => {
-    const wrapper = setup({ setup: true });
+    const wrapper = setup({ success: true });
     const message = findByTestAttr(wrapper, 'congrats-message');
     expect(message.text().length).not.toBe(0)
 });
+
+test('does not throw warning with expected props', () => {
+    const expectedProps = { success: false };
+    checkProps(Congrats, expectedProps);
+})
--- src/test/testUtils.js
+++ src/test/testUtils.js
@@ -1,1 +1,12 @@
+import checkPropTypes from 'check-prop-types'
+
 export const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test="${val}"]`)
+
+export const checkProps = (component, conformingProps) => {
+    const propError = checkPropTypes(
+        component.propTypes,
+        conformingProps,
+        'prop',
+        component.name);
+    expect(propError).toBeUndefined();
+}
